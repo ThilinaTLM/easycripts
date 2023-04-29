cda3e817edd5c7198dd1177d1c0af5fc5a9b0283
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2023-04-29 20:44:12 +0530

    update git scripts

--- git/init-bare-repo.sh
+++ git/init-bare-repo.sh
@@ -1,37 +1,40 @@
-#/bin/bash 
+#!/bin/bash
 
-WORK_DIR=$1
+# Convert the working directory to an absolute path
+WORK_DIR=$(readlink -f "$1")
 if [ -z "$WORK_DIR" ]; then
     echo "Usage: $0 <work_dir>"
     exit 1
 fi
 
-BRANCH=$2
-if [ -z "$BRANCH" ]; then
-    BRANCH=master
-fi
+# Set the branch name, default to 'master' if not provided
+BRANCH=${2:-master}
 
-REPO_DIR=$WORK_DIR/repo.git
-FILES_DIR=$WORK_DIR/files
+# Define repo and files directories
+REPO_DIR="$WORK_DIR/repo.git"
+FILES_DIR="$WORK_DIR/files"
 
-mkdir -p $REPO_DIR
-mkdir -p $FILES_DIR
+# Create directories if they don't exist
+mkdir -p "$REPO_DIR"
+mkdir -p "$FILES_DIR"
 
-# create bare repo
-cd $REPO_DIR
+# Create a bare repo
+cd "$REPO_DIR" || exit
 git init --bare
-cd -
+cd - || exit
 
-# create post receive hook
-cat > $REPO_DIR/hooks/post-receive <<EOF
+# Create a post-receive hook
+cat > "$REPO_DIR/hooks/post-receive" <<EOF
 #!/bin/bash
 
 REPO_DIR=$REPO_DIR
 FILES_DIR=$FILES_DIR
 
-git --work-tree=\$FILES_DIR --git-dir=\$REPO_DIR checkout -f $BRANCH
+git --work-tree="\$FILES_DIR" --git-dir="\$REPO_DIR" checkout -f $BRANCH
 EOF
 
-chmod +x $REPO_DIR/hooks/post-receive
+# Make the post-receive hook executable
+chmod +x "$REPO_DIR/hooks/post-receive"
 
+# Output the location of the bare repo
 echo "Bare repo created at $REPO_DIR"
